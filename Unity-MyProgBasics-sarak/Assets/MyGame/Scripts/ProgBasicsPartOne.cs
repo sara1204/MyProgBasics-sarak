3368d2ce4f4bcc955185609d8f04b67089288f8a
sara1204 <knezevicsara@gmx.at>
2019-03-13 17:41:39 +0100

    scripting

--- Unity-MyProgBasics-sarak/Assets/MyGame/Scripts/ProgBasicsPartOne.cs
+++ Unity-MyProgBasics-sarak/Assets/MyGame/Scripts/ProgBasicsPartOne.cs
@@ -1,16 +1,53 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ProgBasicsPartOne : MonoBehaviour {
 
-	// Use this for initialization
+	int index = 0;
+    int step = 1;
+    
+    // Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
+    
+    // Update is called once per frame
 	void Update () {
+        
+        CountUp(3);
+        Text("mystringText");
+        MultNumbers(3.3f, 1.0f);
+        MixTypes("firstTry", 4, 3.9f);
+    }
+    
+    // Update is called once per frame
+	private void CountUp(){
 		
+        // index = index + 1;   //1.Möglichkeit zum rafzählen
+        //index += 1;             //2.Möglickeit zum raufzählen
+        index = index + step;
+        //index += step;
+        Debug.Log(index);
 	}
+    
+    private void CountUp(int myStep){
+       index = index + myStep;
+    
+        Debug.Log(index);
+    }
+    
+    private void Text(string text){
+        Debug.Log(text);
+    }
+    
+    private void MultNumbers(float first, float second){
+        
+       float tmp = first * second;
+        Debug.Log("MultNumbers: " + tmp);
+    }
+    
+    private void MixTypes(string test, int index, float end){
+        Debug.Log(test + " , " + index + " , " + end); //Konkatination es wird was zusammengefügt
+    }
 }
